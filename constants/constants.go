ced88de51246db7f722a11ada2e9a005d6b06d50
Zachary Huff <zach.huff.386@gmail.com>
2018-06-18 23:58:10 -0400

    Add constants

---
+++ constants/constants.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	Version  = "1.0.937.22"
+	ConfPath = "/etc/pritunl-hsm.json"
+	LogPath  = "/var/log/pritunl-hsm.log"
+	LogPath2 = "/var/log/pritunl-hsm.log.1"
+)
+
+var (
+	Interrupt = false
+)
