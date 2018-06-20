b0f65a9af633590973dc3dffe37bbe1c253a0656
Zachary Huff <zach.huff.386@gmail.com>
2018-06-20 00:17:25 -0400

    Update authority types

--- authority/authority.go
+++ authority/authority.go
@@ -1,29 +1,20 @@
 package authority
 
-type SshPermissions struct {
-	CriticalOptions map[string]string `json:"critical_options"`
-	Extensions      map[string]string `json:"extensions"`
+type SshRequest struct {
+	Type        string `json:"type"`
+	Serial      string `json:"serial"`
+	Certificate []byte `json:"certificate"`
 }
 
-type SshCertificate struct {
-	Key             string         `json:"key"`
-	CertType        uint32         `json:"cert_type"`
-	KeyId           string         `json:"key_id"`
-	ValidPrincipals []string       `json:"valid_principals"`
-	Ttl             int            `json:"ttl"`
-	Permissions     SshPermissions `json:"permissions"`
-}
-
-type SshCsr struct {
-	Type        string         `json:"type"`
-	Serial      string         `json:"serial"`
-	Certificate SshCertificate `json:"certificate"`
+type SshResponse struct {
+	Type        string `json:"type"`
+	Certificate []byte `json:"certificate"`
 }
 
 type HsmPayload struct {
 	Id        string `json:"id"`
 	Token     string `json:"token"`
 	Signature string `json:"signature"`
-	Iv        string `json:"iv"`
-	Data      string `json:"data"`
+	Iv        []byte `json:"iv"`
+	Data      []byte `json:"data"`
 }
